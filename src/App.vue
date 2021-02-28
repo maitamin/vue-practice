64f0bd1ad7583599b512c42dcf821f5d8ff91715
maitamin <3617510+maitamin@users.noreply.github.com>
2021-02-28 12:47:35 +0900

    Vueさわってみた。

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <like-header></like-header>
+    <h2>{{ number }}</h2>
+    <likeNumber :totalNumber="number" @my-click="incrementNumber"></likeNumber>
+    <like-number :total-number="number"></like-number>
+    <like-number></like-number>
+    <!-- これが一番の推奨系ぽい（要素はキャメル、属性はケバブ、イベントはケバブ） -->
+    <likeNumber :total-number="number" @my-click="incrementNumber"></likeNumber>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LikeHeader from './components/LikeHeader';
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      number: 14,
+    }
+  },
   components: {
-    HelloWorld
+    LikeHeader
+  },
+  methods: {
+    incrementNumber(value) {
+      return this.number = value;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
 </style>
